6a560b08dde5ea0579336a1148e7d2848bde7e7a
root <root@ip-172-31-13-77.us-east-2.compute.internal>
2018-08-10 06:03:03 +0000

    1st

---
+++ BCCI/src/main/java/com/ICC/cricket/App.java
@@ -0,0 +1,13 @@
+package com.ICC.cricket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
